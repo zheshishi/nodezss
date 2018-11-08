efcc992c2d16c7f62b855ab74b36a68afc15944b
vanxv <ironvanxv@gmail.com>
2018-11-08 22:21:39 +0800

    url

--- config/config.default.js
+++ config/config.default.js
@@ -1,12 +1,13 @@
 'use strict';
 const path = require('path');
+var weburl = require("./weburl")
 
 module.exports = appInfo => {
   const config = exports = {};
 
   // use for cookie sign key, should change to your own and keep security
   config.keys = appInfo.name + '_1536142102057_2341';
-  config.webUrl = "http://127.0.0.1:7001";
+  config.webUrl = "http:"+weburl+":7001";
   config.security = {
     csrf: {
       enable: false,
@@ -28,7 +29,7 @@ module.exports = appInfo => {
   config.mysql = {
       // database configuration
       client: {
-          host: '127.0.0.1',
+          host: weburl,
           port: '3306',
           user: 'root',
           password: '1121Mysql',
@@ -42,7 +43,7 @@ module.exports = appInfo => {
   config.redis = {
       client: {
           port: 6379,
-          host: '127.0.0.1',
+          host: weburl,
           password: '1121redis',
           db: 1,
       },
@@ -54,12 +55,6 @@ module.exports = appInfo => {
   };
   config.jwt = {
       secret: "1121"
-  };
-  config.webSettings = {
-      url: "http://127.0.0.1:7001"
-  };
-  config.redis = {
-        // your redis configurations
   };
   return config;
 };
---
+++ config/weburl.js
@@ -0,0 +1,2 @@
+var weburl = '192.168.201.106'
+module.exports = weburl
